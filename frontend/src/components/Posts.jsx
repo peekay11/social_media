4ac4d8dc958b2793f4985fa4bedadebdce4aa947
paseka <pasekamabitsela22@gmail.com>
2026-07-19 21:00:27 +0200

    ADDED POSTS

--- frontend/src/components/Posts.jsx
+++ frontend/src/components/Posts.jsx
@@ -1,7 +1,20 @@
 import { memo } from "react";
 import './posts.css';
 
-const posts = [{id: 0,username: 'Konke', caption: "I am alive", time: "just now", pp: "k"}]
+const posts = [
+  { id: 0, username: 'Konke', caption: 'I am alive', time: 'just now', pp: 'k' },
+  { id: 1, username: 'Thabo', caption: 'Coffee first, always.', time: '5m ago', pp: 't_pic' },
+  { id: 2, username: 'Lerato', caption: 'Chasing the sunrise.', time: '10m ago', pp: 'l_pic' },
+  { id: 3, username: 'Sipho', caption: 'Morning routine locked in.', time: '15m ago', pp: 's_pic' },
+  { id: 4, username: 'Zama', caption: 'Blessed to see another day.', time: '20m ago', pp: 'z_pic' },
+  { id: 5, username: 'Chidi', caption: 'Rise and grind!', time: '30m ago', pp: 'c_pic' },
+  { id: 6, username: 'Amina', caption: 'Today is a fresh start.', time: '45m ago', pp: 'a_pic' },
+  { id: 7, username: 'Kofi', caption: 'Good morning world!', time: '1h ago', pp: 'ko_pic' },
+  { id: 8, username: 'Musa', caption: 'Early bird catches the worm.', time: '1h ago', pp: 'm_pic' },
+  { id: 9, username: 'Zola', caption: 'Peaceful mornings hit different.', time: '2h ago', pp: 'zo_pic' },
+  { id: 10, username: 'Neo', caption: 'Manifesting a productive week.', time: '2h ago', pp: 'n_pic' }
+];
+
 
 
 const Post = () => (
